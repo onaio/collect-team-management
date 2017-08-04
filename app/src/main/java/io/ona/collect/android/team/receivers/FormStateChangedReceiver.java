ed122018d086550d372ddb86bd4f1b15248433f4
Jason Rogena <jasonrogena@gmail.com>
2017-08-04 11:02:06 +0300

    FormStateChangedReceiver
    Add the FormStateChangedReceiver class for receiving
    org.odk.collect.android.action.FORM_STATE_CHANGED broadcasts
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

---
+++ app/src/main/java/io/ona/collect/android/team/receivers/FormStateChangedReceiver.java
@@ -0,0 +1,27 @@
+package io.ona.collect.android.team.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by Jason Rogena - jrogena@ona.io on 03/08/2017.
+ */
+
+public class FormStateChangedReceiver extends BroadcastReceiver {
+    private static final String TAG = FormStateChangedReceiver.class.getCanonicalName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive called");
+        if (intent != null && intent.getExtras() != null) {
+            Log.d(TAG, "Intent looks ok");
+            Bundle bundle = intent.getExtras();
+            for (String curKey : bundle.keySet()) {
+                Log.d(TAG, "Intent has key " + curKey + " with value " + bundle.getString(curKey));
+            }
+        }
+    }
+}
